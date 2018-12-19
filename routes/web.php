3af88239b61d5a14ccf0ca441e8f8b60453e15ed
deveron reniers <deveron-reniers@hotmail.com>
2018-12-20 00:18:15 +0100

    setup

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,22 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+
+Route::group(['middle' => 'web'], function () {
+    Route::get('/home', 'HomeController@index');
+    Route::get('/home', 'HomeController@index')->name('home');
+});
+
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'as' => 'admin.'], function () {
+    Route::get('/home', 'HomeController@index')->name('home');
+});
+
+Route::group(['prefix' => 'klant', 'namespace' => 'Auth', 'as' => 'panel.'], function () {
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::get('/order', 'HomeController@index')->name('order');
+    Route::get('/order', 'HomeController@index')->name('home');
+});
+
